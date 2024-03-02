0d6f03f0f3762c8f30aab3dd49926f5fbc190e14
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 23:07:11 -0600

    done again

--- src/get-quote.tsx
+++ src/get-quote.tsx
@@ -1,6 +1,10 @@
 import { Action, ActionPanel, Detail, Icon } from '@raycast/api';
 import { useFetch } from '@raycast/utils';
 
+/* The stoicquotes repository was very helpful in getting this up and running. */
+/* Thank you to rrajath and rajath-ramakrishna-cruise */
+/* https://github.com/raycast/extensions/tree/64ab0ff5e512ef60ccf0e2158828b8cb626bfa01/extensions/stoicquotes/ */
+
 type DailyQuote = {
     content: string;
     author: string;
@@ -10,9 +14,6 @@ const endpoint = 'https://api.quotable.io/random';
 
 export default function Command() {
   const { isLoading, data, revalidate } = useFetch<DailyQuote>(endpoint, {
-    keepPreviousData: false,
-    headers: {
-    },
   });        
 
   const markdownQuote = data ? `> ${data.content}\n\n_${data.author}_` : '';

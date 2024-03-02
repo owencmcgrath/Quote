532e2f25fe90f2f42a7bc605e26c9df4d2a392da
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 22:22:20 -0600

    getting started, should be sleeping

---
+++ src/get-quote.tsx
@@ -0,0 +1,8 @@
+import { Action, ActionPanel, Detail, Icon } from '@raycast/api';
+import { useFetch } from '@raycast/utils';
+
+const endpoint = "https://api.quotable.io";
+
+export default function Command() {
+    
+}

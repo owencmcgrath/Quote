104150de22823661221d864d4f25b073d25500fe
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-03-01 23:04:10 -0600

    Done!

--- src/get-quote.tsx
+++ src/get-quote.tsx
@@ -1,13 +1,49 @@
 import { Action, ActionPanel, Detail, Icon } from '@raycast/api';
 import { useFetch } from '@raycast/utils';
 
-const endpoint = "https://api.quotable.io";
-
-type RandomQuote = {
-    quote: string;
+type DailyQuote = {
+    content: string;
     author: string;
-};
+  };
+
+const endpoint = 'https://api.quotable.io/random';
 
 export default function Command() {
+  const { isLoading, data, revalidate } = useFetch<DailyQuote>(endpoint, {
+    keepPreviousData: false,
+    headers: {
+    },
+  });        
 
+  const markdownQuote = data ? `> ${data.content}\n\n_${data.author}_` : '';
+  const plainTextQuote = data ? `${data.content} - ${data.author}` : '';
+  const dailyQuote = !isLoading && data ? markdownQuote : 'Loading...';
+
+  return (
+    <Detail
+      isLoading={isLoading}
+      markdown={dailyQuote}
+      actions={
+        <ActionPanel>
+          <Action title="New Quote" icon={Icon.ArrowClockwise} onAction={revalidate} />
+          <Action.CopyToClipboard
+            title="Copy as Markdown"
+            content={markdownQuote}
+            shortcut={{
+              modifiers: ['cmd'],
+              key: 'c',
+            }}
+          />
+          <Action.CopyToClipboard
+            title="Copy as Plain Text"
+            content={plainTextQuote}
+            shortcut={{
+              modifiers: ['cmd', 'shift'],
+              key: 'c',
+            }}
+          />
+        </ActionPanel>
+      }
+    />
+  );
 }
